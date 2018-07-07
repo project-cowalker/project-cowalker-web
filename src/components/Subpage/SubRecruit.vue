8f7bc323a5332df0e05e2a54386ba234b10fa10a
rkdud410 <rkdud410@naver.com>
2018-07-07 16:28:26 +0900

    recruit_listview_function

--- src/components/Subpage/SubRecruit.vue
+++ src/components/Subpage/SubRecruit.vue
@@ -1,5 +1,26 @@
 <template>
-    <p>모집부분입니다.</p>
+   <v-container  class="v-container">
+        <v-layout v-for="recruit in allrecruitview" :key="recruit.recruit_idx">
+            <v-flex x12 sm10 md8 lg7 offset-sm1 offset-md2>
+                <v-card flat class="left_card" width="90%"> <!--flat :to="'/boards/' + board.id" class="v-card"-->
+
+                        <v-card-title primary-title>
+                            <v-card flat class="position" width="120px"><h3 class="positon">{{recruit.position}}</h3></v-card>
+                            <div class="number"><h3 class="number_h3">{{recruit.number}}명</h3></div>
+                            <div class="task"><h3>{{recruit.task}}</h3></div>
+                        </v-card-title>
+
+                </v-card>
+            </v-flex>
+
+            <v-flex>
+                <v-card flat class="dday_card" width="60px">
+                   <h3 class="dday">D{{recruit.dday}}</h3>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+
 </template>
 
 <script>
@@ -10,10 +31,80 @@ export default {
   }),
   computed: {
     ...mapGetters({
-      boards: 'allBoards',
+      allrecruitview: 'allrecruitView',
       user: 'user'
     })
 
+  },
+  created () {
+    this.$store.dispatch('recruitingView') // 서버로부터 게시글 다 받아오기
   }
 }
 </script>
+
+<style scoped>
+*{
+    margin: 0;
+    padding: 0;
+}
+
+#home{
+    text-align: center;
+}
+
+header{
+    margin-top: 5vh;
+}
+.left_card {
+    margin-top: 50px;
+    margin-right: 80px;
+    margin-left: 20px;
+    height: 60px;
+    width: 70%;
+    border: 1px solid #64DFFF;
+    border-radius: 10px;
+}
+.dday_card {
+    margin-top: 50px;
+    height: 60px;
+    margin-right: 100px;
+    border: 1px solid #64DFFF;
+    border-radius: 10px;
+}
+h3 {
+  margin-top: 20px;
+}
+#item_list{
+    margin-top: 10vh;
+}
+
+.store{
+    margin-right: .5vw;
+}
+.loadCompleted {
+  font-size: 2rem;
+  text-align: center;
+}
+.loadMore {
+  text-align: center;
+}
+.post-button.btn-sm {
+  width: inherit;
+  padding: 1rem 2rem;
+}
+
+.dday {
+  text-align: center;
+}
+.position {
+  margin-left: 10%;
+}
+.number_h3 {
+  background: rgb(210, 246, 255);
+  border-radius: 20px;
+}
+.task {
+  margin-left: 40%;
+}
+
+</style>
